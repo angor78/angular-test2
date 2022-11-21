424e276f6ad832678099629233ba6dcf2a9ae92d
Angor78 <angor78@gmail.com>
2022-11-21 20:05:01 +0300

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,24 @@ export class AppComponent {
     name: 'Ivan'
   }
   isAppLoading = true
+  text = ''
+  text2 = ''
 
   constructor() {
     setTimeout(() => {
       this.isAppLoading = false
     }, 3000)
+  }
+
+  changeTitleHandler() {
+    this.title = '123'
+  }
+
+  changeTextHandler($event: Event) {
+    this.text = ($event.currentTarget as HTMLInputElement).value
+  }
+
+  changeText2Handler($event: Event) {
+    this.text2 = ($event.currentTarget as HTMLInputElement).value
   }
 }
