4e06ed98cccd1516ff8accde6ad92e03072e2fa3
Angor78 <angor78@gmail.com>
2022-11-22 20:19:17 +0300

    clear tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import {Component} from '@angular/core';
 
 
-interface IUser {
-  age: number
-  name: string
-}
 
 @Component({
   selector: 'app-root',
@@ -12,37 +8,5 @@ interface IUser {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test2';
-  user: IUser = {
-    age: 32,
-    name: 'Ivan'
-  }
-  isAppLoading = true
-  text = ''
-  text2 = ''
-  nums = [1, 2, 3, 4, 5,1]
-  isDuplicate = false
-
-  constructor() {
-    setTimeout(() => {
-      this.isAppLoading = false
-    }, 3000)
-  }
-
-  changeTitleHandler() {
-    this.title = '123'
-  }
-
-  changeTextHandler($event: Event) {
-    this.text = ($event.currentTarget as HTMLInputElement).value
-  }
-
-  changeText2Handler($event: Event) {
-    this.text2 = ($event.currentTarget as HTMLInputElement).value
-  }
-
-  checkIsDuplicate() {
-    let seen: number[] = []
-    this.nums.forEach(num=>!seen.includes(num)?seen.push(num):this.isDuplicate=true)
-  }
+  title=''
 }
