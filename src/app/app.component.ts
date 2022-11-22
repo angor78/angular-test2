c94c5d6d9ba9996df1c9d3fc6dd83df8bccb1033
Angor78 <angor78@gmail.com>
2022-11-22 21:07:30 +0300

    letCode: find missing num

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title=''
+  title: number = 0
+  nums = [0, 1, 2, 4]
+  n = this.nums.length
+
+  findMissingNum() {
+    this.title = this.n * (this.n + 1) / 2 - (this.nums.reduce((el, acc) => acc + el, 0))
+    return this.title
+  }
 }
