83bcbd633a92d001a9704c8f249732cdd2f28dab
Angor78 <angor78@gmail.com>
2022-11-24 19:47:47 +0300

    LeetCode: single num in array with XOR operation

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -9,9 +9,17 @@ import {Grade} from "./child/child.component";
 export class ParentComponent {
   math?: number
   rus?: number
+  nums = [1, 1, 2, 2, 3]
+  single=0
 
   getGrade(value: Grade) {
     this.math = value.math
     this.rus = value.rus
+  }
+
+  singleNum() {
+    let mask = 0
+    this.nums.forEach(num => mask ^= num)
+    this.single=mask
   }
 }
