058157f0f8046647615c8cbefb396e0a112a2039
Angor78 <angor78@gmail.com>
2022-11-26 20:19:20 +0300

    task pause

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,9 +6,25 @@ import {Component} from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
-  grades:Array<string>=['math:5','english:3']
+  grades: Array<string> = ['math:5', 'english:3']
+  clumb: number = 1
 
   getGrade($event: string) {
     this.grades.push($event)
+  }
+
+  // @ts-ignore
+  countClumb() {
+    if (this.clumb === 1) {
+      return 1
+    }
+    let n1:number=1
+    let n2:number=2
+    for (let i=3; i<this.clumb+1; i++){
+      // @ts-ignore
+      n1=n2
+      n2=n1+n2
+    }
+   this.clumb=n2
   }
 }
