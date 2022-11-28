f8f0dc88838cb7c9fac969d8b79225cd07add911
Angor78 <angor78@gmail.com>
2022-11-28 15:16:55 +0300

    LeetCode:Fibonachi method for Climbing stairs

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -7,24 +7,18 @@ import {Component} from '@angular/core';
 })
 export class ParentComponent {
   grades: Array<string> = ['math:5', 'english:3']
-  clumb: number = 1
+  n: number = 1
+  step: number = 0
 
   getGrade($event: string) {
     this.grades.push($event)
   }
 
   // @ts-ignore
-  countClumb() {
-    if (this.clumb === 1) {
-      return 1
-    }
-    let n1:number=1
-    let n2:number=2
-    for (let i=3; i<this.clumb+1; i++){
-      // @ts-ignore
-      n1=n2
-      n2=n1+n2
-    }
-   this.clumb=n2
+  countClumb(n: number) {
+    return this.step = n <= 1
+      ? 1
+      : this.countClumb(n - 1) + this.countClumb(n - 2)
   }
+
 }
