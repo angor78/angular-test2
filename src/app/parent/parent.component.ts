caffb0880836ccc9a17c11cb2edc1e766366163e
Angor78 <angor78@gmail.com>
2022-11-29 21:06:59 +0300

    Angular: directive *ngFor for rendering multiplay array

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,14 +1,54 @@
 import {Component} from '@angular/core';
 
+interface WeekGrades {
+  id: number
+  gradeItem: number
+}
+
+interface Lessons {
+  id: number
+  title: string
+  weekGrades: WeekGrades[]
+}
+
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
-  grades = ['math=5', 'rus=4']
+  Lessons: Lessons[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 2
+        },
+        {
+          id: 1,
+          gradeItem: 3
+        }
+      ]
+    },
+    {
+      id: 1,
+      title: 'Rus',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 5
+        },
+        {
+          id: 1,
+          gradeItem: 4
+        }
+      ]
+    }
+  ]
 
-  getGrade(grade: string) {
-    this.grades.push(grade)
-  }
+  // getGrade(grade: string) {
+    // this.grades.push(grade)
+  // }
 }
