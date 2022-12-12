1500d658bf3b903a1388dd8e4c4f623cbbf620a0
Angor78 <angor78@gmail.com>
2022-12-12 20:14:44 +0300

    Angular: [ngClass] directive

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,5 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
+  isSuccess = true
+  constructor() {
+    setTimeout(()=>{this.isSuccess=false},3000)
 
+  }
 }
