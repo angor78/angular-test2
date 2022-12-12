b451d1b96b86446ea5a225e87ff22df5f1b5089f
Angor78 <angor78@gmail.com>
2022-12-12 20:45:54 +0300

    LeetCode: clear task

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -6,17 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent {
-  prices: Array<number> = [7, 1, 5, 3, 6, 4]
-  maxProfit = 0
-  currentMin = this.prices[0]
-
-  constructor() {
-    this.prices.forEach((price) => {
-      this.maxProfit = Math.max(this.maxProfit, price -this.currentMin)
-      this.currentMin = Math.min(this.currentMin, price)
-    })
-
-  }
-
 
 }
