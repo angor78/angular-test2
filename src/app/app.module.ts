16197e4c4ae1babd625cd5dfcf029f998adfc381
Angor78 <angor78@gmail.com>
2022-12-14 19:02:03 +0300

    Angular: remove components parent and child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {ChildComponent} from './parent/child/child.component';
-import {ParentComponent} from "./parent/parent.component";
+
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
-    ChildComponent,
   ],
   imports: [
     BrowserModule,
