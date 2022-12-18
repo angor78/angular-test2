74c9aaad51a91aab2e7321f85801227c4eb08dfa
Angor78 <angor78@gmail.com>
2022-12-18 16:13:14 +0300

    LeetCode: Maximum subarray

--- src/app/components/comp-b/comp-b.component.ts
+++ src/app/components/comp-b/comp-b.component.ts
@@ -10,6 +10,9 @@ import {CoolLoggerService} from "../../services/cool-logger.service";
 })
 export class CompBComponent implements OnInit {
   value$ = new Observable()
+  nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+  maxSum = this.nums[0]
+  currentSum = this.nums[0]
 
   constructor(private valueService: ValueService,
               private coolLoggerService: CoolLoggerService) {
@@ -21,6 +24,13 @@ export class CompBComponent implements OnInit {
 
   addHandler() {
     this.valueService.add()
-    this.coolLoggerService.log('add','success')
+    this.coolLoggerService.log('add', 'success')
+  }
+
+  getSubArrayHandler() {
+    this.nums.forEach((num) => {
+      this.currentSum = Math.max(this.currentSum + num, num)
+      this.maxSum = Math.max(this.currentSum, this.maxSum)
+    })
   }
 }
