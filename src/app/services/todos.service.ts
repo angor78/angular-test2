3c4f2c2b4a5b99f6050a85f02031ca30d827963c
Angor78 <angor78@gmail.com>
2022-12-19 20:06:13 +0300

    Angular:refactor environment

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 export interface Todo {
   addedDate: string
@@ -21,24 +22,24 @@ export interface BaseResponse<T = {}> {
 export class TodosService {
   httpOptions = {
     withCredentials: true,
-    headers: {'api-key': 'bd4ae310-899e-41ab-b129-c57668c4e43e'}
+    headers: {'api-key': environment.apiKey}
   }
 
   constructor(private http: HttpClient) {
   }
 
   getTodos() {
-    return this.http.get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists',
+    return this.http.get<Todo[]>(`${environment.baseUrl}todo-lists`,
       this.httpOptions)
   }
 
   addTodo() {
-    return this.http.post<BaseResponse<{ item: Todo }>>('https://social-network.samuraijs.com/api/1.1/todo-lists',
+    return this.http.post<BaseResponse<{ item: Todo }>>(`${environment.baseUrl}todo-lists`,
       {title: 'Angular1'}, this.httpOptions)
   }
 
   removeTodo(todoId: string) {
-    return this.http.delete<BaseResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`,
+    return this.http.delete<BaseResponse>(`${environment.baseUrl}todo-lists/${todoId}`,
       this.httpOptions)
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl:'https://social-network.samuraijs.com/api/1.1/',
+  apiKey:'bd4ae310-899e-41ab-b129-c57668c4e43e'
 };
 
 /*
