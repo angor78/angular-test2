262ba89f891f277667d283f7a6a833340cbdca86
Angor78 <angor78@gmail.com>
2022-12-23 17:43:25 +0300

    Angular: clear tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CompAComponent} from './components/comp-a/comp-a.component';
-import {CompBComponent} from './components/comp-b/comp-b.component';
 import {ValueService} from "./services/value.service";
-import {CompCComponent} from './components/comp-c/comp-c.component';
 import {HttpClientModule} from "@angular/common/http";
-import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompAComponent,
-    CompBComponent,
-    CompCComponent,
-    FormComponent,
   ],
   imports: [
     BrowserModule,
