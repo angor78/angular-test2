ad858745d2ef41954ea2c4cf97ed5e403d5c8cad
Angor78 <angor78@gmail.com>
2022-12-24 15:02:20 +0300

    Angular: Links and routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
-import {NotFoundComponent} from "./components/not-found/not-found.component";
 import {UsersComponent} from "./components/users/users.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {TodosComponent} from "./components/todos/todos.component";
@@ -10,7 +9,6 @@ import {TodosComponent} from "./components/todos/todos.component";
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'not-found', component: NotFoundComponent},
   {path: 'users', component: UsersComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'todos', component: TodosComponent}
@@ -20,4 +18,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
