73bed353305edd5a93f537aa62f85805b5fee384
Angor78 <angor78@gmail.com>
2023-01-05 17:34:12 +0300

    Angular: program navigate

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {ProfileResponse, ProfileService} from "../../services/profile.service";
 import {Observable} from "rxjs";
 
@@ -12,15 +12,21 @@ export class ProfileComponent implements OnInit {
 
   profile$!: Observable<ProfileResponse>
 
-  constructor(private route: ActivatedRoute, private profileService: ProfileService) {
+  constructor(
+    private route: ActivatedRoute,
+    private profileService: ProfileService,
+    private router: Router) {
   }
 
   ngOnInit(): void {
     const userId = this.route.snapshot.paramMap.get('userId')
-    if(userId){
-      this.profile$=this.profileService.getUsersProfile(Number(userId))
+    if (userId) {
+      this.profile$ = this.profileService.getUsersProfile(Number(userId))
     }
 
   }
 
+  backToUsersHandler() {
+    this.router.navigate(['/users'])
+  }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -5,22 +5,21 @@ import {Observable} from "rxjs";
 
 export interface ProfileResponse  {
   userId: number
-  lookingForAJob: boolean
-  lookingForAJobDescription: string
-  fullName: string
-  contacts: string
-
-  github: string
-  vk: string
-  facebook: string
-  instagram: string
-  twitter: string
-  website: string
-  youtube: string
-  mainLink: string
+  lookingForAJob?: boolean
+  lookingForAJobDescription?: string
+  fullName?: string
+  contacts?: string
+  github?: string
+  vk?: string
+  facebook?: string
+  instagram?: string
+  twitter?: string
+  website?: string
+  youtube?: string
+  mainLink?: string
   photos: {
-    small: string
-    large: string
+    small: string | null
+    large: string | null
   }
 }
 
